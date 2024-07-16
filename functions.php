179cc6ea75f8f34d94bafc6bef680de42b26a2cd
adous abdessamad <abd.adous@gmail.com>
2024-07-16 22:54:30 +0200

    ajouter la boutton commander dans le header

--- functions.php
+++ functions.php
@@ -10,7 +10,15 @@ if (!defined('ABSPATH')) {
     exit; // 
 }
 
-
+// Fonction pour ajouter le lien "Admin" dans le menu
+function ajouter_lien_admin_dans_menu($items, $args) {
+    // Vérifiez si l'utilisateur est connecté
+    if (is_user_logged_in() ) {
+        // Ajoutez le lien "Admin"
+        $items .= '<li class="menu-item"><a href="' . admin_url() . '">Admin</a></li>';
+    }
+    return $items;
+}
 
 // Accrochez la fonction au hook wp_nav_menu_items
 add_filter('wp_nav_menu_items', 'ajouter_lien_admin_dans_menu', 10, 2);
---
+++ header.php
@@ -0,0 +1,35 @@
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+	<style>
+        body {
+        max-width: 1440px;
+        margin: 0 auto;
+        padding: 0 ;
+    }
+        
+	</style>
+</head>
+<header>
+<?php the_custom_logo(); ?>
+    <div class="container-header">
+	
+	<nav class="menu">
+		<div>
+	
+	</div>
+	<?php wp_nav_menu(array(
+			'theme-location'=>'principal'
+		));?>
+		
+	</nav>
+    </div>
+
+	
+	
+</header>
