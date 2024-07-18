a3b1b6f42ff69633e8d1382c0cd83ee35ff6d9fe
adous abdessamad <abd.adous@gmail.com>
2024-07-18 12:41:34 +0200

    modifier style.css

--- functions.php
+++ functions.php
@@ -10,6 +10,11 @@ if (!defined('ABSPATH')) {
     exit; // 
 }
 
+// Vérifiez que le fichier est appelé dans un contexte WordPress
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
 // Fonction pour ajouter le lien "Admin" dans le menu
 function ajouter_lien_admin_dans_menu($items, $args) {
     // Vérifiez si l'utilisateur est connecté
